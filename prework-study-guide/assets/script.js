10df622c09ee7bec8e126bb8243763267b38a55c
Ryan F <ryanfernandez11@gmail.com>
2022-09-18 20:44:28 -0700

    added JavaScript to project

---
+++ prework-study-guide/assets/script.js
@@ -0,0 +1,18 @@
+var topic = ['HTMLS', 'CSS', 'Git', 'JavaScript'];
+
+for (var i = 0; i < topic.length; i++) {
+    console.log(topic[i]);
+}
+/*
+if (topic === 'HTML') {
+    console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+    console.log("Let's study CSS!");
+} else if (topic === "Git") {
+    console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+    console.log("Let's study JavaScript!");
+} else {
+    console.log('Please try again!');
+}
+*/
